8fb34d928e37763b41f5e27a7098e7bca1ffba46
Anna Sheremetova <annie131187@gmail.com>
2022-10-07 20:40:48 +0700

    task1 completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let number = b**2 - 4 * a * c;
+  if (number === 0) {
+    arr.push(-b / (2 * a));    
+  } else if (number > 0) {
+    arr.push((-b + Math.sqrt(number) )/(2*a));
+    arr.push((-b - Math.sqrt(number) )/(2*a));
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage() {
   let totalAmount;
 
   // код для задачи №2 писать здесь
