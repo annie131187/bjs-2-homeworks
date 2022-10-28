5e0aa08ba0d4e29fc30b662db6cd4e8cc61fd3ca
Anna Sheremetova <annie131187@gmail.com>
2022-10-28 10:10:17 +0700

    tasks1,2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,35 +1,53 @@
+// Задача №1 
+
 function parseCount (value) {
-    let result = Number.parseInt(value);
-    if (result === NaN) {
+    if (!Number(value)) {
         throw new Error('Невалидное значение');
     }
-    return result;
+    return Number.parseInt(value);
 }
 
 function validateCount (value) {
     try {
-    return parseCount(value);
+        return parseCount(value);
     }
     catch (error) {
         return error;
     }
 }
 
+//------------------------------------------------------
+// Задача №2
 
 class Triangle {
     constructor (a, b, c) {
-        this.a = a;
-        this.b = b; 
-        this.c = c;
+        if ((a + b) > c && (a + c) > b && (b + c) > a) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        } else {
+            throw new Error ('Треугольник с такими сторонами не сущетсвует!');
+        }
     }
 
     getPerimeter () {
-        
+        return Object.values(this).reduce((acc, value) => acc + value, 0);
+    }
+
+    getArea () {
+        let p = this.getPerimeter() / 2;
+        let square = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+        return square;
     }
 }
 
-let first = new Triangle(10, 10, 20);
-console.log(first);
-console.log(first.getPerimeter());
+function getTriangle (a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    }
+    catch (error) {
+        return error;
+    }
+}
 
 
